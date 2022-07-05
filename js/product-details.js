028cc42f567462f4f021835fa71daec939dbd503
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-05 16:24:43 -0600

    cambio de imagenes con selector --> 5

--- js/product-details.js
+++ js/product-details.js
@@ -56,7 +56,7 @@ index.search('', {
                                         </div>
                                     </div>
                                     <div id="selector-imgs-products" style="height: 90px; display: flex; justify-content: space-between;">
-                                        <button type="submit">img</button>
+                                        <img src="${hits[0].img_urls[0]}" alt="">
                                     </div>
                                 </div>                                
                             </div>
