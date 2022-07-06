fc6da91243d566f6f5eaf17acb98a194b776c2e0
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-06 09:00:12 -0600

    cambio de price por el de los necesitados  --> 1

--- js/stores.js
+++ js/stores.js
@@ -37,7 +37,7 @@ const HitsRender = (renderOptions, isFirstRender) => {
                             <div class="d-flex align-items-center">
                                 <div class="mb-1 product-price itemprice">
                                     <!--   <span class="fs-5 currencyformat">CAD </span><span class="fs-5">\$${item.bucket_price}</span>-->
-                                    <span class="fs-5 currencyformat">CAD </span><span class="fs-5">\$ ${ (item.available_weights === "each") ? item.price_each : (item.available_weights === "gram") ? item.price_gram : (item.available_weights === "eighth ounce") ? item.price_eighth_ounce : (item.available_weights === "quarter ounce") ? item.price_quarter_ounce : (item.available_weights === "half ounce") ? item.price_half_ounce : (item.available_weights === "ounce") ? item.price_ounce : (item.available_weights === "half gram") ? item.price_half_gram : '00.00'   }</span>
+                                    <span class="fs-5 currencyformat">CAD </span><span class="fs-5">\$ ${ (item.available_weights[0] === "each") ? item.price_each : (item.available_weights[0] === "gram") ? item.price_gram : (item.available_weights[0] === "eighth ounce") ? item.price_eighth_ounce : (item.available_weights[0] === "quarter ounce") ? item.price_quarter_ounce : (item.available_weights[0] === "half ounce") ? item.price_half_ounce : (item.available_weights[0] === "ounce") ? item.price_ounce : (item.available_weights[0] === "half gram") ? item.price_half_gram : '00.00'   }</span>
                                 </div>
                             </div>
                             <div class="product-action mt-2">
