a4136cbd81d8031960b1caceaaabe70a0059cf15
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-06 19:45:51 -0600

    add function add to cart restante logica de add cart --> 4

--- js/stores.js
+++ js/stores.js
@@ -123,17 +123,17 @@ search.start();
 let frame = document.getElementById('jane-menu');
 frame.style = 'display: none;';
 
-window.addEventListener("message", receiveMessage, false);
-
-function receiveMessage(event) {
-    let payload = event.data && event.data.payload;
-    let messageType = event.data && event.data.messageType;
-
-    if (messageType === "loadingEvent" && payload.name === "headlessAppLoaded") {
-        let frame = document.getElementById("jane-menu");
-        frame.contentWindow.postMessage(data, "*");
-    }
-}
+// window.addEventListener("message", receiveMessage, false);
+//
+// function receiveMessage(event) {
+//     let payload = event.data && event.data.payload;
+//     let messageType = event.data && event.data.messageType;
+//
+//     if (messageType === "loadingEvent" && payload.name === "headlessAppLoaded") {
+//         let frame = document.getElementById("jane-menu");
+//         frame.contentWindow.postMessage(data, "*");
+//     }
+// }
 
 
 
