f66bf23f61a16f17b3fea6ce80c4ce0a640b4eaa
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-09 23:46:43 -0600

    Logica de guardado en payload de carrito --> 2

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -1,4 +1,17 @@
 import { data } from '../config/data.js';
+const local_storage = window.localStorage;
+
+
+if (local_storage.getItem('data_product_1') === true) {
+    console.log('Se recibio el item product 1');
+    console.log(JSON.parse(local_storage.getItem('data_product_1')));
+} else if (local_storage.getItem('data_product_2') === true) {
+    console.log('Se recibio el item product 1');
+    console.log(JSON.parse(local_storage.getItem('data_product_2')));
+}
+
+// let item_product_received = local_storage.getItem()
+
 
 window.addEventListener("message", receiveMessage, false);
 
