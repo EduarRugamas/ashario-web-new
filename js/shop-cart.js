c1efb311691931f70759472d3d72e16625091a86
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-10 08:06:12 -0600

    Logica de guardado en payload de carrito --> 8

--- js/shop-cart.js
+++ js/shop-cart.js
@@ -16,7 +16,7 @@ function receiveMessage(event) {
     let payload = event.data && event.data.payload;
     let messageType = event.data && event.data.messageType;
 
-    if (messageType === "analyticsEvent" && payload.name === "cartItemRemoval") {
+    if (messageType === "loadingEvent" && payload.name === "headlessAppLoaded") {
         let frame = document.getElementById("jane-menu");
         frame.contentWindow.postMessage(data, "*");
      } //else if (messageType === "loadingEvent" && payload.name === "headlessAppLoaded") {
