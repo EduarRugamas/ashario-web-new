5854fef6a1eba27b0a2e8313f5277b8a94b33854
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-13 21:23:03 -0600

    loaded hits y ejecutar funcion despues de cargar los hits --> 14

--- js/stores.js
+++ js/stores.js
@@ -50,7 +50,7 @@ const HitsRender = (renderOptions, isFirstRender) => {
                             </div>
                             <div class="product-action mt-2" id="content">
                                <div class="d-grid gap-2">
-                                    <a class="btn btn-dark btn-ecomm" id="add_to_cart_btn" onclick="${ ()=> { console.log('se clickeo'); } }"><i class="bx bxs-cart-add"></i>add to cart</a>
+                                    <a href="${ function () { console.log('se clickeo') } }" class="btn btn-dark btn-ecomm" id="add_to_cart_btn"><i class="bx bxs-cart-add"></i>add to cart</a>
                                     <a href="/views/product-details.html?objectID=${item.objectID}" class="btn btn-light btn-ecomm">Product Details</a>
                                </div> 
                             </div> 
