2d21c54618543c4790774d9c70bc97844f3da539
EduarRugamas <juaneduardo021299@hotmail.com>
2022-07-14 19:33:58 -0600

    minicart --> 24

--- js/stores.js
+++ js/stores.js
@@ -194,6 +194,30 @@ for (let item in cart){
         filters: `product_id:${cart[item].productId} AND store_id:4434`
     }).then( ({hits}) => {
         console.log('item', hits);
+
+        list_items_mini_cart.innerHTML=`
+            <div class="dropdown-item">
+                <div class="d-flex align-items-center">
+                    <div class="flex-grow-1">
+                        <!--<h6 class="cart-product-title">Men White T-Shirt</h6>-->
+                        <!--<p class="cart-product-price">1 X $29.00</p>-->
+                        <h6 class="cart-product-title">${hits[0].name}</h6>
+                        <p class="cart-product-price">1 X $29.00</p>
+                    </div>
+                        <div class="position-relative">
+                        <div class="cart-product-cancel position-absolute">
+                            <i class='bx bx-x'></i>
+                        </div>
+                        <div class="cart-product">
+                            <img src="${hits[0].images_url[0]}" class="" alt="product image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+
+
     }).catch( (error) => {
         console.log('hay un error en la busqueda', error);
     });
