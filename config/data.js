18a08991d9b183595774d158b7a3f64f028aedef
Javier Calderón <javalonso@gmail.com>
2022-07-16 17:15:32 -0600

    no message

--- config/data.js
+++ config/data.js
@@ -2,14 +2,6 @@ export const data = {
     messageType: "buildCart",
     payload: {
         products: [],
-        user: {
-            firstName: "John",   //valores que se reemplazaran por el nombre de la cuenta real
-            lastName: "Smith",
-            birthDate: "1988-04-20",
-            phone: "5556661212",
-            email: "johnsmith@example.com",
-            externalId: "12345",
-        },
         storeId: 4434,
         headlessPartnerName: "AsharioCompany",
         options: {
@@ -19,9 +11,11 @@ export const data = {
                     "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu72xKKTU1Kvnz.woff2",
             },
             theme: {
-                themeColor: "#38b1fa",
+                themeColor: "#000000",
                 navigationColor: "#38b1fa",
                 ctaTextColor: "#ffffff",
+                calloutBackground: '#000000',
+        calloutTextColor: '#FFFFFF',
             },
             redirectUrl: "https://harmonious-mooncake-eb8276.netlify.app/views/shop-grid-left-sidebar.html",
             disableAuthFeatures: true,
