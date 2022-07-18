dcea25dade7708f764c5025fe0ec1b660674e1ee
Javier Calderón <javalonso@gmail.com>
2022-07-17 20:05:52 -0600

    no message

--- js/stores.js
+++ js/stores.js
@@ -343,9 +343,13 @@ search.addWidgets([
     }),
 
 
-        instantsearch.widgets.rangeSlider({
+        instantsearch.widgets.sortBy({
             container: '#container-price',
-            attribute: 'bucket_price',
+            items: [
+    { label: 'Featured', value: 'instant_search' },
+    { label: 'Price (asc)', value: 'instant_search_price_asc' },
+    { label: 'Price (desc)', value: 'instant_search_price_desc' },
+  ],
         }),
 
 
